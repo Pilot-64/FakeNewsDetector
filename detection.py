fcd0811759f35491973dcddb9b6f1116d6bb95dc
Oliver Nederal <oliver.nederal@gmail.com>
2023-02-15 19:20:12 +0100

    work

--- detection.py
+++ detection.py
@@ -1,17 +1,15 @@
-from flask import Flask, jsonify, request
 from tensorflow import keras
 import pandas as pd
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn import preprocessing
 
-app = Flask(__name__)
+from fastapi import FastAPI
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # get the news title from the request
-    X = request.json['title']
+app = FastAPI()
 
+@app.get('/predict')
+def predict(title: str):
     # load the dataset
     model = keras.models.load_model('fakenewsmodel.h5')
     dataraw = pd.read_csv("news.csv")
@@ -27,7 +25,7 @@ def predict():
     tokenizer.fit_on_texts(data['title'])
 
     # tokenize the news title
-    sequences = tokenizer.texts_to_sequences(X)[0]
+    sequences = tokenizer.texts_to_sequences([title])[0]
     sequences = pad_sequences([sequences], maxlen=54,
                               padding='post',
                               truncating='post')
@@ -37,15 +35,12 @@ def predict():
 
     # return the prediction as a JSON response
     if prediction >= 0.8:
-        result = {"prediction": "This news is probably true"}
+        result = "This news is probably true"
     elif prediction >= 0.5:
-        result = {"prediction": "This news could be true"}
+        result = "This news could be true"
     elif prediction <= 0.2:
-        result = {"prediction": "This news is probably fake"}
+        result = "This news is probably fake"
     else:
-        result = {"prediction": "This news could be fake"}
+        result = "This news could be fake"
 
-    return jsonify(result)
-
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0')
+    return result
